7c2413e2848560c7e05e7b9f2eb61c78aa594a86
yakub-pasha2468 <90138046+yakub-pasha2468@users.noreply.github.com>
2021-09-05 18:07:19 +0530

    Add files via upload

---
+++ test_hr.py
@@ -0,0 +1,31 @@
+import time
+
+from selenium import webdriver
+
+
+def test_log_attendance():
+    try:
+        driver = webdriver.Chrome(executable_path=r"C:\just-to-play\hrone\sample\chromedriver.exe")
+        driver.maximize_window()
+        driver.set_page_load_timeout(60)
+        driver.get("https://app.hrone.cloud/login")
+        time.sleep(2)
+        driver.find_element_by_id("hrone-username").send_keys("9849909905")
+        driver.find_element_by_css_selector(".btn-login").click()
+        time.sleep(2)
+        driver.find_element_by_id("hrone-password").send_keys("#Moneyheist@789")
+        time.sleep(2)
+        driver.find_element_by_xpath("//span[text()=' LOG IN ']").click()
+        time.sleep(5)
+        eles = driver.find_elements_by_xpath("//a[text()='May be Later']")
+        if len(eles)>0:
+            eles[0].click()
+        time.sleep(3)
+        element = driver.find_element_by_xpath("//div[contains(@class,'p-dialog-content')]//a[@class='btnclose']")
+        driver.execute_script("arguments[0].click()", element)
+        time.sleep(2)
+        driver.find_element_by_xpath("//button[text()='MARK ATTENDANCE']").click()
+        # driver.find_element_by_xpath("//span[text()=' Mark attendance ']").click()
+    except Exception as e:
+        driver.quit()
+
